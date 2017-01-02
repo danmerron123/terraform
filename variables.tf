b1d100636ef8285660f937d0614ea13adee4a869
danmerron123 <danmerron@gmail.com>
2017-01-02 00:44:43 +0000

    Delete variables.tf

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "access_key" {default = "AKIAJV6IGGCVJ74UMM3A"}
-variable "secret_key" {default = "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"}
-variable "region" {
-  default = "us-east-1"
-}
-variable "key_name" {default = "dan-aws"}
-variable "ATLAS_TOKENn" {default = "jgjzn1I6u4pLVg.atlasv1.WwvMQlylk8QJK5VOOkNWRbjbORdgEYAQSyizT68RTFhxLy43GlzAyF9Pi305RYvX5EU"}
